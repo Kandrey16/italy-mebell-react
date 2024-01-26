ef43d3a674120bb79f219d5bd9fec4d098f2d3ed
Andrey Kurdelov <isip_a.n.kurdelov@mpt.ru>
2024-01-26 23:28:54 +0300

    card

--- src/components/CatalogSection/CatalogCard/CatalogCard.jsx
+++ src/components/CatalogSection/CatalogCard/CatalogCard.jsx
@@ -1,11 +1,11 @@
 import styles from "./CatalogCard.module.scss";
-
+// import { image } from "../assets/images/bedroom.jpg";
 export default function CatalogCard() {
   return (
     <>
       <div className={styles.card}>
         <img
-          src="https://rcnetlabs.com/images/skillset/logos/react.png"
+          src="https://okmebell.ru/image/catalog/Stenki/29,01,2020/stenka-na-zakaz-MO-VO.jpg"
           alt=""
         />
       </div>
--- src/components/CatalogSection/CatalogSection.jsx
+++ src/components/CatalogSection/CatalogSection.jsx
@@ -5,7 +5,7 @@ export default function CatalogSection() {
     <>
       <section className="container mx-auto">
         <h1 className="text-center">Каталог</h1>
-        <div className="grid grid-cols-4 gap-4 bg-orange-200">
+        <div className="grid grid-cols-4 gap-12 my-4">
           <CatalogCard></CatalogCard>
           <CatalogCard></CatalogCard>
           <CatalogCard></CatalogCard>
