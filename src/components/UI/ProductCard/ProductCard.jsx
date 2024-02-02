b4f08f44fa1286b738b49eafef300b740f64c364
Andrey Kurdelov <isip_a.n.kurdelov@mpt.ru>
2024-02-02 11:37:00 +0300

    v.1.01

--- src/components/UI/ProductCard/ProductCard.jsx
+++ src/components/UI/ProductCard/ProductCard.jsx
@@ -1,5 +1,8 @@
 import styles from "./ProductCard.module.scss";
 import image from "images/chair.jpg";
+import arrow_logo from "@/assets/Arrow.svg";
+import favourite_logo from "@/assets/favourite.svg";
+import cart_logo from "@/assets/cart_2.svg";
 
 export default function ProductCard() {
   return (
@@ -11,8 +14,19 @@ export default function ProductCard() {
         <div className={styles.product_info}>
           <h2 className={styles.product_name}>Стул Marokko</h2>
           <p className={styles.product_price}>5000$</p>
-          <div>{/* Здесь должны быть цветовые варианты */}</div>
-          <a className={styles.product_link}>Подробнее</a>
+          <div className={styles.product_options}>
+            <div className={styles.product_link}>
+              <a>Подробнее</a>
+              <img src={arrow_logo}></img>
+            </div>
+            <button>
+              <img src={favourite_logo}></img>
+            </button>
+          </div>
+          <button className={styles.product_button}>
+              <img src={cart_logo}></img>
+              <p>В корзину</p>
+          </button>
         </div>
       </div>
     </>
